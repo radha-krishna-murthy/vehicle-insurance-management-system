6daf01855f2951e7c4aedf4b8659bc445f0361e4
radha-krishna-murthy <trkm212@gmail.com>
2025-02-04 16:49:41 +0530

    Update NewCustomer.java

--- vehicle insurance/src/vehicle/insurance/NewCustomer.java
+++ vehicle insurance/src/vehicle/insurance/NewCustomer.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
  
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this liense
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 
